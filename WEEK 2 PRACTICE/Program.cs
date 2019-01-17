f68fa32f2876b19a8b569c9908ed89da849c835f
visualstudio <visualstudio@230-PC35>
2019-01-17 15:33:33 -0500

    THIS IS NEW CODE

---
+++ WEEK 2 PRACTICE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEEK_2_PRACTICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
